fa751bad3a025e2e659b0e41fd1f44277f915222
RoboSurok <isaenkovaleks@mail.ru>
2025-02-02 21:08:01 +0300

    Add files via upload
    Добавлена программа для базы данных

---
+++ gen_data_log.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+con = sqlite3.connect('Project_Electrician/data_log.db')
+
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS data_log 
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            skin TEXT, 
+            wins INTEGER)''')
+
+[print(el) for el in cur.execute('SELECT * FROM data_log').fetchall()]
+
+cur.execute('''DELETE FROM data_log''')
+
+con.commit()
+
+cur.close()
