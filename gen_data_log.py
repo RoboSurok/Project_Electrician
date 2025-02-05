328ca4e78e31222204d676065751539e01ce988f
RoboSurok <isaenkovaleks@mail.ru>
2025-02-05 16:03:09 +0300

    Update gen_data_log.py
    PEP8

--- gen_data_log.py
+++ gen_data_log.py
@@ -4,9 +4,9 @@ con = sqlite3.connect('Project_Electrician/data_log.db')
 
 cur = con.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS data_log 
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            skin TEXT, 
+cur.execute('''CREATE TABLE IF NOT EXISTS data_log
+            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            skin TEXT,
             wins INTEGER,
             lose INTEGER)''')
 
